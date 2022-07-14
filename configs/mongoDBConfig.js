ec5c3e840a7eebf9c580a8f84bea34ca2adcb641
Daniel Tran <danieltran770@gmail.com>
2022-07-13 22:01:48 -0400

    added mongoDBConfig

---
+++ configs/mongoDBConfig.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const dbUrl = process.env.DB_URL || process.env.DB_DEV_URL;
+
+mongoose.connect(dbUrl);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
--- models/order.js
+++ models/order.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 const orderSchema = new Schema({
+	name: { type: String, required: true },
 	email: { type: String, required: true },
 	phone: { type: String, required: true },
 	date: { type: Date, default: Date.now },
